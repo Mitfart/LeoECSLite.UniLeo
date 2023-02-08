3d502ea227311da56a7e76312a8e8c3babec3a94
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-02-08 20:05:11 +0300

    Update ConvertToEntity.cs

--- Runtime/ConvertToEntity.cs
+++ Runtime/ConvertToEntity.cs
@@ -2,46 +2,49 @@ using Leopotam.EcsLite;
 using UnityEngine;
 
 namespace Mitfart.LeoECSLite.UniLeo{
-   [DisallowMultipleComponent]
-   public class ConvertToEntity : MonoBehaviour{
-      [field: SerializeField] public string WorldName { get; private set; }
+  [DisallowMultipleComponent]
+  public class ConvertToEntity : MonoBehaviour{
+    [field: SerializeField] public string WorldName { get; private set; }
       
-      public EcsWorld        World        { get; private set; }
-      public EcsPackedEntity PackedEntity { get; private set; }
-      public bool            IsConverted  { get; private set; }
+    public EcsWorld        World        { get; private set; }
+    public EcsPackedEntity PackedEntity { get; private set; }
+    public bool            IsConverted  { get; private set; }
 
       
 
-      private void Start() {
-         Convert(EcsWorldsLocator.Get(WorldName));
-      }
+    private void Start() {
+      Convert();
+    }
 
 
 
-      public EcsPackedEntity Convert() => Convert(World);
+    public EcsPackedEntity Convert() => Convert(EcsWorldsLocator.Get(WorldName));
       
-      public EcsPackedEntity Convert(EcsWorld world) {
-         if (IsConverted) return PackedEntity;
+    public EcsPackedEntity Convert(EcsWorld world) {
+      if (IsConverted) return PackedEntity;
 
-         var entity = world.NewEntity();
-         foreach (var provider in GetComponents<IConvertToEntity>()) 
-            provider.Convert(entity, world);
+      World = world;
+         
+      var entity = World.NewEntity();
+      foreach (var provider in GetComponents<IConvertToEntity>()) 
+        provider.Convert(entity, World);
 
-         IsConverted = true;
-         return PackedEntity = world.PackEntity(entity);
-      }
+      IsConverted  = true;
+      PackedEntity = World.PackEntity(entity);
+      return PackedEntity;
+    }
       
       
       
-      public bool TryGetEntity(out int e){
-         return PackedEntity.Unpack(World, out e);
-      }
+    public bool TryGetEntity(out int e) {
+      return PackedEntity.Unpack(World, out e);
+    }
 
 
 
-      private void OnValidate() {
-         if (string.IsNullOrWhiteSpace(WorldName)) 
-            WorldName = string.Empty;
-      }
-   }
+    private void OnValidate() {
+      if (string.IsNullOrWhiteSpace(WorldName)) 
+        WorldName = string.Empty;
+    }
+  }
 }
