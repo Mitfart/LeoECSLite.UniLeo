c4a6b0b5a9e69ed02ddfc794f9311e976cab3bd0
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-02-08 17:09:42 +0300

    Update ConvertSceneSys.cs

--- Runtime/ConvertSceneSys.cs
+++ Runtime/ConvertSceneSys.cs
@@ -4,6 +4,7 @@ using Object = UnityEngine.Object;
 namespace Mitfart.LeoECSLite.UniLeo {
   public class ConvertSceneSys : IEcsPreInitSystem {
     public void PreInit(IEcsSystems systems) {
+      EcsWorldsLocator.Register(null, systems.GetWorld());
       EcsWorldsLocator.RegisterAllFrom(systems);
       
       foreach (var convertable in Object.FindObjectsOfType<ConvertToEntity>()) {
